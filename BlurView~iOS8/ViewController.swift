b4089bb6187bd53defe01398772e95e585a668f6
Daston <vinodh.g.dev@gmail.com>
2014-07-06 15:16:50 +0530

    Initial Commit

---
+++ BlurView~iOS8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlurView~iOS8
+//
+//  Created by Vinodh  on 06/07/14.
+//  Copyright (c) 2014 Daston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
